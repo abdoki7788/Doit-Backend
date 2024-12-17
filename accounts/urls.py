ef53d23211b9eb8d8c736d700e6bc04bea1fe659
abdoki7788 <ad.kiany.2009@gmail.com>
2024-12-17 20:36:51 +0330

    ...

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,10 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path, include
-from .views import UserRegisterView
+from .views import UserRegisterView, UserDetailView
 
 urlpatterns = [
     path('token/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('users/register/', UserRegisterView.as_view(), name='user_register_view'),
-    path('users/me/', UserRegisterView.as_view(), name='me_view')
+    path('users/me/', UserDetailView.as_view(), name='me_view')
 ]
